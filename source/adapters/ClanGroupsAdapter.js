f4416e787c5b2d25770900aedc10db3dc7ac26fa
tmetz <tammy@tammymetz.com>
2019-11-26 14:47:23 -0800

    create fetch request to get list of clan groups

--- source/adapters/ClanGroupsAdapter.js
+++ source/adapters/ClanGroupsAdapter.js
@@ -2,4 +2,9 @@ class ClanGroupsAdapter {
     constructor() {
         this.baseURL = 'http://localhost:3000/api/v1/clan_groups'
     }
+
+    getGroups() {
+        return fetch(this.baseURL).then(res => res.json()
+        )
+    }
 }
