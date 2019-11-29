cb4d85810a6a61244a56f7deac6604421628c8e5
tmetz <tammy@tammymetz.com>
2019-11-29 15:44:53 -0800

    fetch all 4 clans in constructIntro

--- src/components/intro.js
+++ src/components/intro.js
@@ -5,11 +5,29 @@ class Intro {
 
     constructIntro() {
         const motherClanID = document.getElementById("mother").childNodes[0].id.slice(5)
+        const fatherClanID = document.getElementById("father").childNodes[0].id.slice(5)
+        const maternalGrandpaClanID = document.getElementById("maternal-grandpa").childNodes[0].id.slice(5)
+        const paternalGrandpaClanID = document.getElementById("paternal-grandpa").childNodes[0].id.slice(5)
         const clanStrings = []
         this.adapter
         .getClan(motherClanID)
         .then(clan => {
-            clanStrings.push(clan.dine_bizaad_name + ' nishłį́')
+            clanStrings[0] = clan.dine_bizaad_name + ' nishłį́'
+        })
+        this.adapter
+        .getClan(fatherClanID)
+        .then(clan => {
+            clanStrings[1] = clan.dine_bizaad_name + ' bashishchiin'
+        })
+        this.adapter
+        .getClan(maternalGrandpaClanID)
+        .then(clan => {
+            clanStrings[2] = clan.dine_bizaad_name + ' dashicheii'
+        })
+        this.adapter
+        .getClan(paternalGrandpaClanID)
+        .then(clan => {
+            clanStrings[3] = clan.dine_bizaad_name + ' dashinalí'
         })
         console.log(clanStrings)
     }
@@ -21,10 +39,6 @@ class Intro {
 Yá’át’ééh (Hello). 
 Then they move into their name.
 
-Shí éí (name) yinishyé (I am called ... )
-Father's clan bashishchiin
+Shí éí (name) yinishyé (I am called ... ) 
 
-Maternal grandfather's clan dashicheii
-
-Paternal grandfather's clan dashinalí
 */
