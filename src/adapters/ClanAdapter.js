2588f1177ed56ae46d72ed0570acd297bef74762
tmetz <tammy@tammymetz.com>
2019-11-29 18:24:17 -0800

    create ClanAdapter to send patch request to update clan count

---
+++ src/adapters/ClanAdapter.js
@@ -0,0 +1,18 @@
+class ClanAdapter {
+    constructor() {
+        this.baseURL = 'http://localhost:3000/api/v1/clans/'
+    }
+
+    updateClanCount(clanID) {
+        let configurationObject = {
+            method: "PATCH"
+            headers: {
+                "Content-Type": "application/json"
+                "Accept": "application/json"
+            }
+        };
+        
+        return fetch(this.baseURL + `${clanID}`, configurationObject).then(res => res.json()
+        )
+    }
+}
